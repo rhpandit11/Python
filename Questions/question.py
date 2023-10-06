5ed0d06f7b438c63ec59af84e0e0e0e3a5fd9e28
rhpandit11 <rh.pandit11@gmail.com>
2023-10-06 13:03:32 +0530

    update

---
+++ Questions/question.py
@@ -0,0 +1,23 @@
+# Question:
+# Write a program which can compute the factorial of a given numbers.
+# The results should be printed in a comma-separated sequence on a single line.
+# Suppose the following input is supplied to the program:
+# 8
+# Then, the output should be:
+# 40320
+
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+try:
+    num = int(input("Enter a number: "))
+    result = factorial(num)
+    print(result)
+except ValueError:
+    print("Please enter a valid integer.")
