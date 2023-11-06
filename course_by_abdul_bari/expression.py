4132ff484efa63c3c198658b991f6bc44fb71a7f
rhpandit11 <rh.pandit11@gmail.com>
2023-11-06 12:08:16 +0530

    update

---
+++ course_by_abdul_bari/expression.py
@@ -0,0 +1,8 @@
+# Area OF A Rectangle
+
+length = int(input("Enter length of a rectangle "))
+breadth = int(input("Enter breadth of a rectangle "))
+
+area = length * breadth
+
+print("Area is ", area)
