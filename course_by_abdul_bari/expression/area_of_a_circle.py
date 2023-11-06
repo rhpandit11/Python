7496cd9cf2b794eef0ae824a3450b2fb4c84492b
rhpandit11 <rh.pandit11@gmail.com>
2023-11-06 12:52:06 +0530

    update

---
+++ course_by_abdul_bari/expression/area_of_a_circle.py
@@ -0,0 +1,8 @@
+#Area of a circle
+import math
+
+radius = float(input("Enter radius"))
+
+area = math.pi * radius ** 2
+
+print("area is", area)
---
+++ course_by_abdul_bari/expression/converting_kms_to_miles.py
@@ -0,0 +1,7 @@
+#converting kms to miles
+
+km = float(input("Enter km "))
+miles = km * 0.621371
+
+print("Miles = ", miles)
+
