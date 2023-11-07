d185072783b35f0a70b8a0035833d5737ceec63b
rhpandit11 <rh.pandit11@gmail.com>
2023-11-07 14:06:27 +0530

    update

--- course_by_abdul_bari/conditional_statements/marks_subjects.py
+++ course_by_abdul_bari/conditional_statements/marks_subjects.py
@@ -1,3 +1,5 @@
+#check if a statement has passed or failed , by taking marks in 3 subjects .
+
 math = int(input("Enter your math marks: "))
 physics = int(input("Enter your physics marks: "))
 chemistry = int(input("Enter your chemistry marks: "))
