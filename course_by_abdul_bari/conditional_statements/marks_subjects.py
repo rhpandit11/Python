492f50a4b1a0aaea77fb1f6a51cf26dbac943b0b
rhpandit11 <rh.pandit11@gmail.com>
2023-11-07 14:05:54 +0530

    update

---
+++ course_by_abdul_bari/conditional_statements/marks_subjects.py
@@ -0,0 +1,8 @@
+math = int(input("Enter your math marks: "))
+physics = int(input("Enter your physics marks: "))
+chemistry = int(input("Enter your chemistry marks: "))
+
+if math >= 45 and physics >= 45 and chemistry >= 45:
+    print("passed ")
+else:
+    print("failed")
