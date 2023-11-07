79219c191c1edc86559468a79147c23b55603623
rhpandit11 <rh.pandit11@gmail.com>
2023-11-07 13:43:37 +0530

    update

---
+++ course_by_abdul_bari/conditional_statements/find_difference_between_two_numbers.py
@@ -0,0 +1,7 @@
+number_one = int(input("Enter Numbe One: "))
+number_two = int(input("Enter Numbe Two: "))
+
+if number_one - number_two >= 0:
+    print(number_one-number_two)
+else:
+    print(number_two-number_one)
