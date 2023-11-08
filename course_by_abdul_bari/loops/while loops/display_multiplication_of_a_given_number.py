260d0180d461dd964d00f6e30b1df8760dfc0f75
rhpandit11 <rh.pandit11@gmail.com>
2023-11-08 15:20:07 +0530

    update

--- course_by_abdul_bari/loops/while loops/display_multiplication_of_a_given_number.py
+++ course_by_abdul_bari/loops/while loops/display_multiplication_of_a_given_number.py
@@ -1,6 +1,7 @@
-n = int(input("Enetr enter a number: "))
+n = int(input("Eneter a number: "))
 counter = 1
 
 while counter <= 10:
-    print(n,'x',counter,'=', n*counter)
+    print(n,'x',counter,'=',n*counter)
+
     counter = counter + 1
