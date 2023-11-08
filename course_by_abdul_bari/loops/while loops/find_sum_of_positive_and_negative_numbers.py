3786327cabf424b018ccc0a4124ebdb2ddcdafa9
rhpandit11 <rh.pandit11@gmail.com>
2023-11-08 21:29:23 +0530

    update

--- course_by_abdul_bari/loops/while loops/find_sum_of_positive_and_negative_numbers.py
+++ course_by_abdul_bari/loops/while loops/find_sum_of_positive_and_negative_numbers.py
@@ -1,10 +1,15 @@
 num_of_numbers = int(input("Enter a num_of_numbers: "))
-sum = 0
+psum = 0
+nsum = 0
 count = 0
 
 while count < num_of_numbers:
     n = int(input("Enter a number: "))
-    sum = sum + n
+    if n > 0:
+        psum = psum + n
+    else:
+        nsum = nsum +n
+
     count = count + 1
 
-print(sum)
+print("Positive numbers sum ara: ",psum,"Negative numbers sum are: ",nsum)
