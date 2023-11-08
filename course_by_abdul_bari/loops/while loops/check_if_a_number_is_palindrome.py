87430215189c39ec42dee1d2280d606ff5fc30ed
rhpandit11 <rh.pandit11@gmail.com>
2023-11-08 15:55:08 +0530

    update

--- course_by_abdul_bari/loops/while loops/check_if_a_number_is_palindrome.py
+++ course_by_abdul_bari/loops/while loops/check_if_a_number_is_palindrome.py
@@ -1,12 +1,12 @@
 n = int(input("Enter a number: "))
-m = n
+m = n #storing the original value to n for further comparision
 rev = 0
 
 while n > 0:
     r = n % 10
     n = n // 10
     rev = rev * 10 + r
-if m == rev:
+if m == rev: #comparing the number to check
         print("Palindrome")
 else:
       print("Not Palindrome")
