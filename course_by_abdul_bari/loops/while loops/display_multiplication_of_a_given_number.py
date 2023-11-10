abdd88362a1a36272ffe4cb6252a568673e290a7
rhpandit11 <rh.pandit11@gmail.com>
2023-11-10 15:50:15 +0530

    update

--- course_by_abdul_bari/loops/while loops/display_multiplication_of_a_given_number.py
+++ course_by_abdul_bari/loops/while loops/display_multiplication_of_a_given_number.py
@@ -1,7 +1,7 @@
-n = int(input("Eneter a number: "))
-counter = 1
+n = int(input("Eneter a number: ")) #taking a number from user ex-5
+counter = 1 #starting counter from one (5 * 1 = 5)
 
-while counter <= 10:
-    print(n,'x',counter,'=',n*counter)
+while counter <= 10: # condition of while is, the counter value should be <= 0
+    print(n,'x',counter,'=',n*counter) #displaying multiplication table of given number
 
-    counter = counter + 1
+    counter = counter + 1 #increasing value of counter
