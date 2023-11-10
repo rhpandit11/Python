9e70692d976b0a9efa70994022eb85c1f2af0b7a
rhpandit11 <rh.pandit11@gmail.com>
2023-11-10 16:11:43 +0530

    update

--- course_by_abdul_bari/loops/while loops/find_the_maximum_numbers_from _the_given_numbers.py
+++ course_by_abdul_bari/loops/while loops/find_the_maximum_numbers_from _the_given_numbers.py
@@ -1,4 +1,4 @@
-num_of_numbers = int(input("Enter num_of_numbers: "))
+num_of_numbers = int(input("Enter num_of_numbers: ")) # how many number of times you want to enter numbers
 max = 0
 count = 0
 
@@ -6,7 +6,7 @@ max =   int(input("Enter a number: ")) #if we are taking minus numbers as input
 
 while count < num_of_numbers - 1:
     n = int(input("Enter a number: "))
-    if n > max:
+    if n > max:        # if user have entered a number which is greater than max than take max = n
         max = n
     count = count + 1
 
