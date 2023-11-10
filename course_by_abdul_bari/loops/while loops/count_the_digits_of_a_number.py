b8a3486a584d6a2c378c8d0506f8a467163757a8
rhpandit11 <rh.pandit11@gmail.com>
2023-11-10 15:53:03 +0530

    update

--- course_by_abdul_bari/loops/while loops/count_the_digits_of_a_number.py
+++ course_by_abdul_bari/loops/while loops/count_the_digits_of_a_number.py
@@ -1,6 +1,6 @@
-n = int(input("Enter a number: "))
-count = 0
-while n > 0:
-    n = n // 10
-    count = count + 1
-print("Number of digits are :",count)
+n = int(input("Enter a number: ")) # taking input from user 
+count = 0 # counter starting from 0
+while n > 0: # while input number is greater than zero  
+    n = n // 10 # divide it by 10 and make it into int using // floor division
+    count = count + 1 # modify counter value
+print("Number of digits are :",count) # print the output
