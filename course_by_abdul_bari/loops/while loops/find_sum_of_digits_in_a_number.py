1def70e81b47074cba29f0050a1cf1ab9bb4536e
rhpandit11 <rh.pandit11@gmail.com>
2023-11-10 15:55:28 +0530

    update

--- course_by_abdul_bari/loops/while loops/find_sum_of_digits_in_a_number.py
+++ course_by_abdul_bari/loops/while loops/find_sum_of_digits_in_a_number.py
@@ -1,7 +1,7 @@
-n = int(input("Enter a number: "))
-sum = 0
-while n > 0:
-    r = n % 10
-    n = n // 10
-    sum = sum + r
+n = int(input("Enter a number: ")) # taking input from user
+sum = 0 # start sum value from 0
+while n > 0: # while input number is greater than zero
+    r = n % 10 # divide given number by 10
+    n = n // 10 # divide it by 10 and make it into int using //
+    sum = sum + r # modify counter value
 print("Sum of digits are :",sum)
