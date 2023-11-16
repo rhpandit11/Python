af83da742dcbff0c48b4ae6d8492e6fc0cde53e4
rhpandit11 <rh.pandit11@gmail.com>
2023-11-16 18:47:28 +0530

    update

---
+++ course_by_abdul_bari/strings/convert_string_to_palindrome.py
@@ -0,0 +1,5 @@
+str1 = input("Enter a name")
+
+rev = str1[::-1]
+
+print(str1 + rev)
---
+++ course_by_abdul_bari/strings/palindrome_or_not.py
@@ -0,0 +1,8 @@
+str1 = input("Enter a name")
+
+rev = str1[::-1]
+
+if str1 == rev:
+    print('palindrome')
+else:
+    print('Not')
