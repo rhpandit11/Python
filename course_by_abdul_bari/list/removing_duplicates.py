0157ec598286171710c52b3c5c98cfb1e6f35845
rhpandit11 <rh.pandit11@gmail.com>
2023-11-21 23:15:28 +0530

    update

--- course_by_abdul_bari/list/removing_duplicates.py
+++ course_by_abdul_bari/list/removing_duplicates.py
@@ -11,3 +11,19 @@ for x in L1:
     if x not in L2:
         L2.append(x)
 print(L2)
+
+
+# printing list without creating new list
+
+L1 = [3,5,7,9,3,6,5,2,3,7,10]
+
+# index = 0
+
+# while index < len(L1):
+#     current_element = L1[index]
+#     if L1.count(current_element) > 1:
+#         L1.remove(current_element)
+#     else:
+#         index += 1
+
+# print(L1)
